a793d3c90c745a9f987b4405dc7fa6ce630996c5
palmwi2010 <wop3000@hotmail.co.uk>
2024-07-23 22:33:13 +0100

    completed exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,21 @@
-const leapYears = function() {
+const leapYears = function(yr) {
 
+    if (typeof yr != 'number') return "ERROR";
+    if (yr < 0) return "ERROR";
+
+    if (yr % 4 === 0) {
+        if (yr % 100 === 0) {
+            if (yr % 400 === 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
